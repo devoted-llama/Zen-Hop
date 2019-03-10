c3118122e43fbaeac693fa1a36f9f2aab9816600
Devoted Llama <devotedllamadev@gmail.com>
2019-03-10 00:02:54 +0000

    New button for controlling angle and power.

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -5,77 +5,89 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Frog : MonoBehaviour {
+    public static Frog instance = null;
 
     public Text powerAmountText;
     public GameObject aimer;
     public float powerForceMultiplier;
     float powerAmount = 0;
 
-    public Slider powerSlider;
+    public Button powerButton;
 
-    void Start() {
-        SetPower ();
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy (gameObject);
+        }
+
+        DontDestroyOnLoad (gameObject);
     }
 
-    void Update() {
-        if (!EventSystem.current.IsPointerOverGameObject()) {
-            if (Input.GetButtonUp ("Fire1")) {
-                GetComponent<Rigidbody2D> ().AddForce (GetForce());
-                //powerAmount = 0;
-                UpdateAmountText ();
-            }
 
 
-            if (Input.GetButton ("Fire1")) {
-                Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+    void Start() {
+        SetPower (0);
+    }
 
-                float adjacent = mousePos.x - transform.position.x;
-                float opposite = mousePos.y - transform.position.y;
+    public void Jump() {
+        GetComponent<Rigidbody2D> ().AddForce (GetForce());
+        UpdateAmountText ();
+    }
 
-                adjacent = adjacent < 0 ? 0 : adjacent;
-                opposite = opposite < 0 ? 0 : opposite;
-
-                //Debug.LogFormat ("{0},{1}", adjacent, opposite);
-
-                float angle = Mathf.Rad2Deg * Mathf.Atan (opposite / adjacent);
-
-                float hypotenuse = opposite / Mathf.Sin (Mathf.Deg2Rad*angle);
-
-                //Debug.LogFormat ("h: {0}, Dist: {1}, sin: {2}, angle: {3}", hypotenuse, Vector3.Distance(mousePos,transform.position),Mathf.Sin (Mathf.Deg2Rad*angle), angle);
-            
-                if (!float.IsNaN (angle)) {
-                    Quaternion qAngle = aimer.transform.localRotation;
-                    Vector3 eulerAngle = qAngle.eulerAngles;
-                    eulerAngle.z = 90 - angle;
-                    qAngle.eulerAngles = eulerAngle;
-                    aimer.transform.localRotation = qAngle;
-
-                    /*if (!float.IsNaN(hypotenuse)) {
-                        powerAmount = hypotenuse * powerForceMultiplier;
-                        powerAmount = powerAmount > 100 ? 100 : powerAmount;
-                        UpdateAmountText ();
-                    }*/
-                }
-            }
+    public void SetAimerAngle(float angle, float modifier = 0) {
+        if (!float.IsNaN (angle)) {
+            Quaternion qAngle = aimer.transform.localRotation;
+            Vector3 eulerAngle = qAngle.eulerAngles;
+            eulerAngle.z = modifier + angle;
+            qAngle.eulerAngles = eulerAngle;
+            aimer.transform.localRotation = qAngle;
         }
     }
 
-    Vector2 GetForce() {
-        float ratio = aimer.transform.localEulerAngles.z / 90f;
 
-        Vector2 force = new Vector2 (ratio*powerAmount*powerForceMultiplier, (1 - ratio)*powerAmount*powerForceMultiplier);
+    Vector2 GetForce() {
+        float angle = aimer.transform.localEulerAngles.z;
+        float div = 90f;
+
+        float ratio = 0f;
+        float forceX = 0f;
+        float forceY = 0f;
+
+        if (angle >= 0 && angle <= 90) {
+            ratio = aimer.transform.localEulerAngles.z / 90f;
+            forceX = ratio * powerAmount;
+            forceY = (1 - ratio) * powerAmount;
+        } else if (angle > 90 && angle <= 180) {
+            ratio = (aimer.transform.localEulerAngles.z - 90) / 90f;
+            forceX = (1 - ratio)* powerAmount;
+            forceY = -(ratio) * powerAmount;
+        } else if (angle > 180 && angle <= 270) {
+            ratio = (aimer.transform.localEulerAngles.z - 180) / 90f;
+            forceX = -ratio * powerAmount;
+            forceY = -(1 - ratio) * powerAmount;
+        } else if (angle > 270 && angle <= 360) {
+            ratio = (aimer.transform.localEulerAngles.z - 270) / 90f;
+            forceX = -(1-ratio) * powerAmount;
+            forceY = ratio * powerAmount;
+        }
+
+        Debug.LogFormat ("x: {0}, y: {1}", forceX, forceY);
+
+        Vector2 force = new Vector2 (forceX, forceY);
 
         return force;
        
     }
 
-    public void SetPower() {
-        powerAmount = powerSlider.value * powerSlider.maxValue;
+    public void SetPower(float power) {
+        powerAmount = power * powerForceMultiplier;
+        powerAmount = powerAmount > 1000 ? 1000 : powerAmount;
         UpdateAmountText ();
     }
 
     void UpdateAmountText() {
-        powerAmountText.text = powerAmount.ToString ("F0") + "%";
+        powerAmountText.text = (powerAmount/10f).ToString ("F0");
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class UIController : MonoBehaviour {
+    GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    EventSystem m_EventSystem;
+	// Use this for initialization
+	void Start () {
+        m_Raycaster = GetComponent<GraphicRaycaster>();
+        m_EventSystem = GetComponent<EventSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton ("Fire1")) {
+            m_PointerEventData = new PointerEventData (m_EventSystem);
+            m_PointerEventData.position = Input.mousePosition;
+            List<RaycastResult> results = new List<RaycastResult> ();
+            if (m_Raycaster != null) {
+                m_Raycaster.Raycast (m_PointerEventData, results);
+                foreach (RaycastResult result in results) {
+                    //Debug.LogFormat ("centre: {0}, pos: {1}",result.gameObject.transform.position,Camera.main.ScreenToWorldPoint(result.screenPosition));
+                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(result.screenPosition);
+                    float adjacent = mousePos.x - result.gameObject.transform.position.x;
+                    float opposite = mousePos.y - result.gameObject.transform.position.y;
+                    
+                    float angle = Mathf.Rad2Deg * Mathf.Atan ( adjacent / opposite);
+
+                    Vector2 point1 = mousePos;
+                    Vector2 point2 = result.gameObject.transform.position;
+
+                    //Debug.LogFormat ("h: {0}, Dist: {1}, sin: {2}, angle: {3}", hypotenuse, Vector2.Distance(point1,point2),Mathf.Sin (Mathf.Deg2Rad*angle), angle);
+
+                    float modifier = 0;
+
+                    if (adjacent >= 0 && opposite >= 0) {
+                        modifier = 0;
+                    } else if (adjacent > 0 && opposite < 0) {
+                        modifier = 180f;
+                    } else if (adjacent < 0 && opposite < 0) {
+                        modifier = 180f;
+                    } else if (adjacent < 0 && opposite > 0) {
+                        modifier = 360f;
+                    }
+
+                    Frog.instance.SetAimerAngle (angle, modifier);
+                    Debug.Log (Vector2.Distance (point1, point2));
+                    Frog.instance.SetPower (Vector2.Distance(point1,point2));
+                }
+
+            }
+        }
+
+        if (Input.GetButtonUp ("Fire1")) {
+            m_PointerEventData = new PointerEventData (m_EventSystem);
+            m_PointerEventData.position = Input.mousePosition;
+            List<RaycastResult> results = new List<RaycastResult> ();
+            if (m_Raycaster != null) {
+                m_Raycaster.Raycast (m_PointerEventData, results);
+                foreach (RaycastResult result in results) {
+                    Frog.instance.Jump ();
+                }
+
+            }
+        }
+    }
+}
