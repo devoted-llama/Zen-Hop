24b2672be9be8280e3890786b8fa5ba16f34c1aa
Daniel <danlevy2013@gmail.com>
2019-03-16 20:49:32 +0000

    Add new gameover panel. Cloud in out transitions.

--- Assets/Scripts/HillsController.cs
+++ Assets/Scripts/HillsController.cs
@@ -7,7 +7,7 @@ public class HillsController : MonoBehaviour {
     public Transform sprite1;
     public Transform sprite2;
 
-    const float width = 18.57f;
+    const float width = 26.00f;
     const float parallaxSpeed = 0.4f;
     const float scrollSpeed = 0.06f;
 
